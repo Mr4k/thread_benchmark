6641432b217e777f8146973537450b6d572ccece
Peter Stefek <peterlikesemail@gmail.com>
2020-12-12 12:42:29 -0800

    hmmm

---
+++ distributed_counter/src/distributed_counter.rs
@@ -0,0 +1,21 @@
+use std::sync::{RwLock};	
+
+pub struct LockingCounter {	
+    pub counts: Vec<RwLock<u64>>,	
+}	
+
+impl LockingCounter {	
+    pub fn increment(&self, thread_id: usize) {	
+        let lock = &self.counts[thread_id*20];	
+        let mut val = lock.write().unwrap();	
+        *val += 1;	
+    }	
+    pub fn fetch(&self) -> u64 {	
+        let mut sum: u64 = 0;	
+        for thread in &self.counts {	
+            let count =  thread.read().unwrap();	
+            sum += count.clone();	
+        }	
+        return sum	
+    }	
+}
--- distributed_counter/src/main.rs
+++ distributed_counter/src/main.rs
@@ -1,9 +1,9 @@
-use std::{sync::Arc, sync::{Mutex}, thread, thread::JoinHandle};
+use std::{sync::Arc, sync::{RwLock}, thread, thread::JoinHandle};
 use std::env;
 
-mod locking_counter;
+mod distributed_counter;
 
-use locking_counter::LockingCounter;
+use distributed_counter::LockingCounter;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -11,23 +11,20 @@ fn main() {
     let num_threads: usize = args[1].parse().unwrap();
     let num_writes: usize = args[2].parse().unwrap();
     
-    let counters: Vec<Arc<LockingCounter>> = (0..num_threads).map(|_| { Arc::new(LockingCounter{count: Mutex::new(0)}) }).collect();
+    let counter = Arc::new(LockingCounter{counts: (0..(num_threads * 20)).map(|_| { RwLock::new(0) }).collect()});
     
-    let handles: Vec<JoinHandle<u64>> = (0..num_threads).map(|thread_id| {
-        let counter = Arc::clone(&counters[thread_id]);
-        let res = thread::spawn(move || {
+    let handles: Vec<JoinHandle<_>> = (0..num_threads).map(|thread_id| {
+        let counter  = Arc::clone(&counter);
+        thread::spawn(move || {
             for _ in 0..num_writes {
-                counter.increment();
+                counter.increment(thread_id);
             }
-            counter.fetch()
-        });
-        res
+        })
     }).collect();
 
-    let mut sum = 0;
     for h in handles {
-        sum += h.join().unwrap();
+        h.join().unwrap();
     }
 
-    assert_eq!(sum, (num_threads * num_writes) as u64)
+    assert_eq!(counter.fetch(), (num_threads * num_writes) as u64)
 }
